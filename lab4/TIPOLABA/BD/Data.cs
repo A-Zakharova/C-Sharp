33afc848a8e730f6b13d98cffff9cc5143c7655b
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-12-22 21:24:50 +0300

    Add files via upload

---
+++ lab4/TIPOLABA/BD/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIPOLABA
+{
+    public class Data
+    {
+        public string ConnectionString { get; set; }
+        public string LoggerConnectionString { get; set; }
+        public string SourceDirectory { get; set; }
+        public string TargetDirectory { get; set; }
+    }
+}
